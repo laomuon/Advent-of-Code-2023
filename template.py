d232661da0763d85b0e44980ee0a8943a5336655
laomuon <laomuon@github.com>
2023-12-10 03:03:33 +0100

    Add template file

---
+++ template.py
@@ -0,0 +1,17 @@
+def part1(filename: str) -> int:
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            pass
+
+
+def part2(filename: str) -> int:
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            pass
+
+
+if __name__ == "__main__":
+    print(f"Part 1 example: {part1('example')}")
+    print(f"Part 1 result: {part1('input')}")
+    print(f"Part 2 result: {part2('example')}")
+    print(f"Part 2 result: {part2('input')}")
